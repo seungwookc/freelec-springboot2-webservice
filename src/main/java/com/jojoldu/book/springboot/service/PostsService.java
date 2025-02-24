c1c6efad1a92b8f286b7c5fcfd47c9413d70698e
user <sochoi0832@naver.com>
2025-02-25 00:28:19 +0900

    commit

--- src/main/java/com/jojoldu/book/springboot/service/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -42,5 +42,6 @@ public class PostsService {
     @Transactional
     public void delete(Long id){
         Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다 id=" + id));
+        postsRepository.delete(posts);
     }
 }
